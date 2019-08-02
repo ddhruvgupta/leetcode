37420a116abd585e43e4cc0de2e08ec3329eb1d1
dhruv_CM <dhruv.gupta@codemettle.com>
2019-08-02 13:04:28 -0400

    comments on Palindrome finder added

--- LeetCode/src/longestPalindrome.java
+++ LeetCode/src/longestPalindrome.java
@@ -10,8 +10,10 @@ public class longestPalindrome {
 		
 		int start = 0, end = 0;
 		for(int i=0;i<s.length();i++) {
-			int len1 = expand(s,i,i);
-			int len2 = expand(s,i,i+1);
+			//Start from first char and expand outward to check if longer palindrome is possible
+			
+			int len1 = expand(s,i,i); //handle case for odd length Palindrome
+			int len2 = expand(s,i,i+1); //handle case doe even length Palindrome
 			
 			int len = Math.max(len1,len2);
 			if(len> end-start) {
--- LeetCode/src/test.java
+++ LeetCode/src/test.java
@@ -4,7 +4,7 @@ public class test {
 
 	public static void main(String[] args) {
 		
-		String test = " - ";
-		System.out.println(atoi.test(test));
+//		mostWater.test();
+		longestPalindrome lp = new longestPalindrome();
 	}
 }
